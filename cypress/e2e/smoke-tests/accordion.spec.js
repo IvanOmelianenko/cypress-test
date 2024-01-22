76877c8cceeda89d578828194148f2eb8260c451
Ivan <vanyokredray@gmail.com>
2024-01-22 14:14:00 +0200

    hw7_lesson_13

--- cypress/e2e/smoke-tests/accordion.spec.js
+++ cypress/e2e/smoke-tests/accordion.spec.js
@@ -2,7 +2,7 @@
 
 describe("Accordion text test", () => {});
 
-it("Verify accordion text after opening", () => {
+it("Verify accordion text after opening it", () => {
   cy.visit("/pages/layout/accordion");
   cy.get("nb-card-body button").click();
   cy.get(".item-body").first().should("include.text", "A nebula");
